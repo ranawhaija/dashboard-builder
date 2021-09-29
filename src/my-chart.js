c211d553d1a859240d3837bd5f097e73fdc0f58d
rhaija <rana@disruptk.com>
2021-09-29 12:27:35 +0300

    add annotations to the code

--- src/my-chart.js
+++ src/my-chart.js
@@ -1,9 +1,17 @@
 
+/**
+ * MyChart class to use the portal builder
+ *
+ * @class MyChart
+ * @author Rana Abul-Haija
+ */
 class MyChart {
+    // declare private attributes
     #portalChartsData;
     #portalContainerId;
 
     constructor(portalContainerId, portalChartsData) {
+        // load the dependency Chart.js library
         dynamicallyLoadScript('../dependencies/chartjs.js').then(() => {
             this.#portalChartsData = portalChartsData;
             this.#portalContainerId = portalContainerId;
@@ -13,10 +21,22 @@ class MyChart {
         });
     }
 
+    /**
+     * return passed portal data
+     *
+     * @readonly
+     * @memberof MyChart
+     */
     get portalChartsData() {
         return this.#portalChartsData;
     }
 
+    /**
+     * build the structure of the portal
+     *
+     * @private
+     * @memberof MyChart
+     */
     #buildPortal() {
         let priorityArray = new Array(this.#portalChartsData.length);
         let col = 1;
@@ -30,13 +50,16 @@ class MyChart {
         background-color: rgba(0,0,0,0.1);
         padding: 10px;`);
 
+        // sort the charts by order in case they are not,
+        // then set its position in the grid
         this.#portalChartsData.sort((a, b) => {
             return a.order - b.order;
         }).forEach((chartData, index) => {
+            let chartElement = document.createElement("div");
+            // check  if the current row has a width capacity for new chart
             if (6 - col < chartData.width - 1) {
                 col = 1;
             }
-            let chartElement = document.createElement("div");
             parentNode.appendChild(chartElement);
             if (chartElement.isConnected) {
                 if (chartData.type == 'html-content') {
@@ -54,6 +77,7 @@ class MyChart {
                     chartElement.appendChild(chartCanvas);
 
                     if (chartCanvas.isConnected) {
+                        // set pririty charts array
                         priorityArray[chartData.priority - 1] = {
                             canvas: chartCanvas,
                             config: {
@@ -72,19 +96,29 @@ class MyChart {
 
                     }
                 }
+                // update the col value to place the next chart 
                 col = col + chartData.width;
             }
         });
+        // fetch and render the charts based on its priority
         this.#fetchChartData(priorityArray);
     }
 
+    /**
+     * fetch the data of the portal
+     *
+     * @private
+     * @memberof MyChart
+     */
     #fetchChartData(priorityArray) {
         priorityArray.forEach((chart) => {
-
+            // check if the data is based as is or by url,
+            // if it is a url then fetch the data using http request
             if (typeof (chart.config.data) == 'string') {
                 var xmlHttp = new XMLHttpRequest();
                 xmlHttp.onreadystatechange = () => {
                     if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                        // check if the json text is valid and can be parsed
                         try {
                             chart.config.data = JSON.parse(xmlHttp.responseText);
                             this.#buildChart(chart);
@@ -93,7 +127,7 @@ class MyChart {
                         }
                     }
                 }
-                xmlHttp.open("GET", chart.config.data, true);
+                xmlHttp.open("GET", chart.config.data, true); // async request
                 xmlHttp.send(null);
             } else {
                 this.#buildChart(chart);
@@ -101,10 +135,17 @@ class MyChart {
         });
     }
 
+    /**
+     * build the chart
+     *
+     * @private
+     * @memberof MyChart
+     */
     #buildChart(chart) {
         if (chart.config.type == 'html-content') {
             this.#buildHtmlContentChart(chart);
         } else {
+            // using chart.js function
             new Chart(
                 chart.canvas,
                 chart.config
@@ -112,6 +153,13 @@ class MyChart {
         }
     }
 
+
+    /**
+     * build the widget of type html content
+     *
+     * @private
+     * @memberof MyChart
+     */
     #buildHtmlContentChart(chart) {
         chart.canvas.setAttribute('style', `
         display: flex;
@@ -153,7 +201,13 @@ class MyChart {
     }
 }
 
-
+/**
+ * load the script and resolve only if the dependency loaded
+ * 
+ * @param url String
+ * @returns Promise
+ *
+ */
 function dynamicallyLoadScript(url) {
     var script = document.createElement("script");  // create a script DOM node
     script.src = url;  // set its src to the provided URL
